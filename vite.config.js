ace66d135a71f72f4654f5561bcd43e791aa6ae7
Ayush <ayush39174113@gmail.com>
2025-07-10 22:09:38 +0530

    updated server configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',  
-    port: parseInt(process.env.PORT) || 5173
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: 'all' 
   }
 })
